26e0d0af30bc6a4f0e23f0a9a2870ea81c3ae08c
kyorohiro <kyorohiro@gmail.com>
2018-11-08 04:47:49 +0000

    add show request/response

--- app/api/message_api.rb
+++ app/api/message_api.rb
@@ -11,7 +11,19 @@ class MessageAPI < Grape::API
         Message.new(title: "yamada", content: "game"),
         Message.new(title: "yamada", content: "game")]
     end
+
+    desc "response message list"
+    params do
+      requires :id
+    end
+    route_param :id do
+      get do
+         Message.new(title: "yamada", content: "game")
+      end
+    end
   end
+  
+
 end
 
 
